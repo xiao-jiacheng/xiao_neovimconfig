57b9f01f6240070d96dcd68a18f423dc62b8f88b
xiaojc2 <xiaojc2@lenovo.com>
2023-08-07 11:30:37 +0800

    bufferline 配置

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,11 +8,24 @@ vim.api.nvim_set_keymap("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", {norema
 vim.api.nvim_set_keymap("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>-", ":BufferLinePickClose<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>bg", ":BufferLinePick", {noremap = true, silent = true})
 
 
 vim.opt.termguicolors = true
-require("bufferline").setup{}
-
+require("bufferline").setup{
+    options = {
+        -- 使用 nvim 内置lsp
+        diagnostics = "coc",
+        numbers = "ordinal",
+        -- 左侧让出 nvim-tree 的位置
+        offsets = {{
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left"
+        }}
+    }
+}
 
 
